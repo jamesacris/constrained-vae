50d0db5053d9aae8b6f5d942faf9489294c49bd9
James Acris <james.acris@stfc.ac.uk>
2021-05-11 09:39:12 +0100

    CNN BVAE 20epoch reconstructions

--- dSprites/vis.py
+++ dSprites/vis.py
@@ -3,15 +3,31 @@ import tensorflow_datasets as tfds
 import os
 import matplotlib.pyplot as plt
 
+# Configure gpu
+gpus = tf.config.list_physical_devices("GPU")
+print(gpus)
+if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)],
+        )
+        logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
+
 from helpers import subplot_image
 from beta_vae_model import latent_dim
 from data_prep import dataset
 
 # load encoder & decoder
-encoder = tf.keras.models.load_model("./models/BVAE_encoder_dense_50ep")
+encoder = tf.keras.models.load_model("./models/BVAE_encoder_cnn_20epochs_4.0beta")
 encoder.summary()
 
-decoder = tf.keras.models.load_model("./models/BVAE_decoder_dense_50ep")
+decoder = tf.keras.models.load_model("./models/BVAE_decoder_cnn_20epochs_4.0beta")
 decoder.summary()
 
 
@@ -52,5 +68,5 @@ decoded_imgs = decoder.predict(
 plt.figure(dpi=100, figsize=(ncols * 2, nrows * 2.2))
 for iplot in range(nrows * ncols):
     subplot_image(decoded_imgs[iplot, :, :], "", nrows, ncols, iplot)
-plt.savefig("./figs/BVAE/reconstructions_dense_50epoch.png")
+plt.savefig("./figs/BVAE/reconstructions_dense_cnn_20epochs_4beta.png")
 plt.show()
