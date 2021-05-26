c0645fd86a78e58dd697a0b039f0d541be0028cd
James Acris <james.acris@stfc.ac.uk>
2021-05-26 10:34:58 +0100

    train a model only using first gpu with memory limit

--- dSprites_beta_VAE/train_beta_VAE.py
+++ dSprites_beta_VAE/train_beta_VAE.py
@@ -5,20 +5,20 @@ from dsprites_data import get_dsprites_tf_dataset
 # set gpu memory limit, gradually reduce to test demands
 import tensorflow as tf
 
-gpus = tf.config.list_physical_devices('GPU')
-print(gpus)
-if gpus:
-  # Restrict TensorFlow to only allocate some of memory on the first GPU
-  try:
-    tf.config.experimental.set_virtual_device_configuration(
-        gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*24)])
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Virtual devices must be set before GPUs have been initialized
-    print(e)
+physical_gpus = tf.config.list_physical_devices('GPU')
+tf.config.set_visible_devices(physical_gpus[0], 'GPU')
+logical_gpus = tf.config.experimental.list_logical_devices('GPU')
 
+# Restrict TensorFlow to only allocate some of memory on the first GPU
+tf.config.experimental.set_virtual_device_configuration(
+    logical_gpus[0],
+    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*24)])
+
+print(len(physical_gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+print(logical_gpus)
+
+tf.config.set_soft_device_placement(True)
+tf.debugging.set_log_device_placement(True)
 
 # dateset
 dset = get_dsprites_tf_dataset()
@@ -30,15 +30,10 @@ norm_beta = 0.002
 # create bvae
 bvae = DspritesBetaVAE(latent_dim=latent_dim, normalized_beta=norm_beta, random_seed=0)
 
-# don't let tf auto assign jobs to devices and log
-tf.debugging.set_log_device_placement(True)
-tf.config.set_soft_device_placement(False)
 
-
-with tf.device('/device:GPU:0'):
-  # train and save
-  # save_dir: where to save all results (use None for automatic dir)
-  # batch_limit_for_debug: how many batches to use per epoch (for quick debug)
-  #                        set batch_limit_for_debug=None to use all batches
-  bvae.train_save(dset, epochs=20, batch_size=256, lr=.01, save_dir='output_train/orig_architecture/20ep/',
-                  verbose_batch=50, verbose_epoch=1, batch_limit_for_debug=1)
+# train and save
+# save_dir: where to save all results (use None for automatic dir)
+# batch_limit_for_debug: how many batches to use per epoch (for quick debug)
+#                        set batch_limit_for_debug=None to use all batches
+bvae.train_save(dset, epochs=20, batch_size=256, lr=.01, save_dir='output_train/orig_architecture/20ep/',
+                verbose_batch=50, verbose_epoch=1, batch_limit_for_debug=1)
