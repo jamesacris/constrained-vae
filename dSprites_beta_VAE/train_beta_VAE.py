f462d0c5da885ea6ef0d199db762cf6fd2edda5a
James Acris <james.acris@stfc.ac.uk>
2021-05-26 14:08:10 +0100

    parse args

--- dSprites_beta_VAE/train_beta_VAE.py
+++ dSprites_beta_VAE/train_beta_VAE.py
@@ -1,43 +1,31 @@
-from itertools import product
+from argparse import ArgumentParser
 from dsprites_beta_VAE import DspritesBetaVAE
 from dsprites_data import get_dsprites_tf_dataset
 
-# set gpu memory limit, gradually reduce to test demands
-import tensorflow as tf
+parser = ArgumentParser()
+parser.add_argument("-z", "--nlat", default=10, type=int, help="dimension of latent space")
+parser.add_argument("--norm_beta", default=0.002, type=float, help="normalised beta")
+parser.add_argument("--seed", default=0, type=int, help="random seed to initialise model weights")
+parser.add_argument("--epochs", default=1, type=int, help="number of training epochs")
+parser.add_argument("-b", "--batch-size", default=256, type=int, help="size of mini-batch")
+parser.add_argument("-l", "--learning-rate", default=.01, type=float, help="learning rate")
+parser.add_argument("--savedir", default=None, help="where to save all results (use None for automatic dir)")
+parser.add_argument("--verbose-batch", default=1, type=int, help="frequency with which to log batch metrics")
+parser.add_argument("--verbose-epoch", default=1, type=int, help="frequency with which to log epoch metrics")
+parser.add_argument("--batch-lim-debug", default=None, help="how many batches to use per epoch (for quick debug)")
+args = parser.parse_args()
 
-memory = 4096 + 512
-
-physical_gpus = tf.config.list_physical_devices('GPU')
-print(physical_gpus)
-# Restrict TensorFlow to only allocate some of memory on the first GPU
-tf.config.experimental.set_virtual_device_configuration(
-    physical_gpus[0],
-    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory)])
-tf.config.set_visible_devices(physical_gpus[0], 'GPU')
-logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-print(logical_gpus)
-
-
-print(len(physical_gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-print(logical_gpus)
-
-tf.config.set_soft_device_placement(True)
-tf.debugging.set_log_device_placement(True)
-
-# dateset
+# dataset
 dset = get_dsprites_tf_dataset()
 
-# use params with max latent size (biggest memory demand)
-latent_dim = 200
-norm_beta = 0.002
-
 # create bvae
-bvae = DspritesBetaVAE(latent_dim=latent_dim, normalized_beta=norm_beta, random_seed=0)
+bvae = DspritesBetaVAE(latent_dim=args.nlat, normalized_beta=args.norm_beta, random_seed=args.seed)
 
 
 # train and save
 # save_dir: where to save all results (use None for automatic dir)
 # batch_limit_for_debug: how many batches to use per epoch (for quick debug)
 #                        set batch_limit_for_debug=None to use all batches
-bvae.train_save(dset, epochs=20, batch_size=256, lr=.01, save_dir=f'output_train/testing_memory/{memory}_MB/',
-                verbose_batch=1, verbose_epoch=1, batch_limit_for_debug=1)
+bvae.train_save(dset, epochs=args.epochs, batch_size=args.batch_size, lr=args.learning_rate, 
+                save_dir=args.savedir, verbose_batch=args.verbose_batch, 
+                verbose_epoch=args.verbose_epoch, batch_limit_for_debug=args.batch_lim_debug)
