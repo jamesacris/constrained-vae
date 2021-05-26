1a58169f8a224e35549f4c5b36370ae50927f140
james-cloud-vm <james.acris@stfc.ac.uk>
2021-05-26 10:03:03 +0000

    memory demand 5GB

--- dSprites_beta_VAE/train_beta_VAE.py
+++ dSprites_beta_VAE/train_beta_VAE.py
@@ -5,14 +5,18 @@ from dsprites_data import get_dsprites_tf_dataset
 # set gpu memory limit, gradually reduce to test demands
 import tensorflow as tf
 
-physical_gpus = tf.config.list_physical_devices('GPU')
-tf.config.set_visible_devices(physical_gpus[0], 'GPU')
-logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+memory = 1024*5
 
+physical_gpus = tf.config.list_physical_devices('GPU')
+print(physical_gpus)
 # Restrict TensorFlow to only allocate some of memory on the first GPU
 tf.config.experimental.set_virtual_device_configuration(
-    logical_gpus[0],
-    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*24)])
+    physical_gpus[0],
+    [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory)])
+tf.config.set_visible_devices(physical_gpus[0], 'GPU')
+logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+print(logical_gpus)
+
 
 print(len(physical_gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 print(logical_gpus)
@@ -35,5 +39,5 @@ bvae = DspritesBetaVAE(latent_dim=latent_dim, normalized_beta=norm_beta, random_
 # save_dir: where to save all results (use None for automatic dir)
 # batch_limit_for_debug: how many batches to use per epoch (for quick debug)
 #                        set batch_limit_for_debug=None to use all batches
-bvae.train_save(dset, epochs=20, batch_size=256, lr=.01, save_dir='output_train/orig_architecture/20ep/',
-                verbose_batch=50, verbose_epoch=1, batch_limit_for_debug=1)
+bvae.train_save(dset, epochs=20, batch_size=256, lr=.01, save_dir=f'output_train/testing_memory/{memory}_MB/',
+                verbose_batch=1, verbose_epoch=1, batch_limit_for_debug=1)
