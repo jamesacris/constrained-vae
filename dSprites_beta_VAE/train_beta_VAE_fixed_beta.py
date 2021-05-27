7df5942868662cbac33d3b260f270c0002894999
james-vae-cloud-vm <james.acris@stfc.ac.uk>
2021-05-27 16:01:36 +0000

    vgpu config debugging

--- dSprites_beta_VAE/train_beta_VAE_fixed_beta.py
+++ dSprites_beta_VAE/train_beta_VAE_fixed_beta.py
@@ -8,7 +8,11 @@ import multiprocessing
 def train_func(vGPU, nlat, norm_beta, seed, epochs, batch_size, learning_rate,
                savedir, verbose_batch, verbose_epoch, batch_lim_debug):
     
+    print(f"training model with latent dim = {nlat}")
+
     with tf.device(vGPU):
+        print(f"assigned device {vGPU}")
+
         # create bvae
         bvae = DspritesBetaVAE(latent_dim=nlat, normalized_beta=norm_beta, random_seed=seed)
 
@@ -22,7 +26,7 @@ def train_func(vGPU, nlat, norm_beta, seed, epochs, batch_size, learning_rate,
                         verbose_epoch=verbose_epoch, batch_limit_for_debug=batch_lim_debug)
 
 
-if "name" == "__main__":
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--norm_beta", default=0.002, type=float, help="normalised beta")
     parser.add_argument("--seed", default=0, type=int, help="random seed to initialise model weights")
@@ -34,9 +38,6 @@ if "name" == "__main__":
     parser.add_argument("--verbose-epoch", default=1, type=int, help="frequency with which to log epoch metrics")
     parser.add_argument("--batch-lim-debug", default=None, type=int, help="how many batches to use per epoch (for quick debug)")
     args = parser.parse_args()
-
-    # dataset
-    dset = get_dsprites_tf_dataset()
 
     # create virtual GPU
     # 5 GB x 20 = 100 GB
@@ -56,13 +57,18 @@ if "name" == "__main__":
 
     # each GPU ==> 5 virtual vGPUs
     print(vGPUs)
-    assert False
+
+    # dataset
+    dset = get_dsprites_tf_dataset()
 
     args_pool = []
-    for i, n_lat in enumerate(range(10, 201, 10)):
-        args = (vGPUs[i].name, n_lat, args.norm_beta, args.seed, args.epochs, args.batch_size, 
+    for i, n_lat in enumerate(range(10, 21, 10)):
+        _args = (vGPUs[i].name, n_lat, args.norm_beta, args.seed, args.epochs, args.batch_size, 
                 args.learning_rate, args.savedir, args.verbose_batch, args.verbose_epoch, args.batch_lim_debug)
+        args_pool.append(_args)
+    print(args_pool)
+
     
-    with multiprocessing.Pool(processes=20) as pool:
-        pool.starmap(train_func, args)
+    with multiprocessing.Pool(processes=2) as pool:
+        pool.starmap(train_func, args_pool)
     
