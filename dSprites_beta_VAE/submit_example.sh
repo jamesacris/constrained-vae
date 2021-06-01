c2740f5720bdb996d669c865bbd5898b1bc00b08
James Acris <james.acris@stfc.ac.uk>
2021-06-01 11:28:51 +0100

    debug limit

--- dSprites_beta_VAE/submit_example.sh
+++ dSprites_beta_VAE/submit_example.sh
@@ -1,10 +1,10 @@
-python train_beta_VAE.py -z 10 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 20 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 30 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 40 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 50 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0
-python train_beta_VAE.py -z 60 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 70 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 80 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 90 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
-python train_beta_VAE.py -z 100 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --physical-gpu 0 &
+python train_beta_VAE.py -z 10 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 20 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 30 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 40 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 50 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0
+python train_beta_VAE.py -z 60 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 70 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 80 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 90 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
+python train_beta_VAE.py -z 100 --norm_beta .5 --epoch 20 --verbose-batch 1000000 --batch-lim-debug 5 --physical-gpu 0 &
