70c1e144ddc90e81059cbc73436e2485e54104f8
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2021-06-27 19:17:44 +0100

    Update compute_disentangle_metric.py

--- dSprites_beta_VAE/compute_disentangle_metric.py
+++ dSprites_beta_VAE/compute_disentangle_metric.py
@@ -30,7 +30,6 @@ if __name__ == "__main__":
     njobs = len(args.norm_beta_list) * len(args.nlat_list)
     assert njobs == comm.Get_size()
     # cpu threads
-    tf.config.threading.set_inter_op_parallelism_threads(args.num_threads)
     tf.config.threading.set_intra_op_parallelism_threads(args.num_threads)
     # device
     if args.disable_gpu:
